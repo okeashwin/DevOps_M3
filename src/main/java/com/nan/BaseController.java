7ff47eafc3e694f15b8ad4d9ea63fff9ec0c4f81
nkdalmia <nkdalmia@users.noreply.github.com>
2015-11-18 15:34:13 -0500

    feature flags set,get,list APIs

--- src/main/java/com/nan/BaseController.java
+++ src/main/java/com/nan/BaseController.java
@@ -6,17 +6,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import redis.clients.jedis.Jedis;
 
+import java.util.Map;
+
 @Controller
 public class BaseController {
+
+    public static final String FEATURES_MAP_KEY = "features";
+    public static final String FLAG_OFF = "off";
+    public static final String FLAG_ON = "on";
+    public static final String REDIS_HOST = "localhost";
+    public static final int REDIS_PORT = 6379;
 
     @RequestMapping("/feature/set")
     public ModelAndView setFlag(
             @RequestParam(value = "name", required = true) String name,
             @RequestParam(value = "flag", required = true) String flag) {
         ModelAndView mv = new ModelAndView("feature_set");
-        flag = "off".equalsIgnoreCase(flag) ? "off" : "on";
-        Jedis jedis = new Jedis("localhost", 6379);
-        jedis.set("feature_" + name, flag);
+        flag = FLAG_OFF.equalsIgnoreCase(flag) ? FLAG_OFF : FLAG_ON;
+        Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT);
+        jedis.hset(FEATURES_MAP_KEY, name, flag);
         String message = "Successfully switched " + flag + " feature " + name;
         mv.addObject("message", message);
         return mv;
@@ -26,8 +34,8 @@ public class BaseController {
     public ModelAndView getFlag(
             @RequestParam(value = "name", required = true) String name) {
         ModelAndView mv = new ModelAndView("feature_get");
-        Jedis jedis = new Jedis("localhost", 6379);
-        String flag = jedis.get("feature_" + name);
+        Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT);
+        String flag = jedis.hget(FEATURES_MAP_KEY, name);
         String message;
         if (flag == null) {
             message = "Cannot find feature with name" + name;
@@ -37,6 +45,15 @@ public class BaseController {
         }
 
         mv.addObject("message", message);
+        return mv;
+    }
+
+    @RequestMapping("/feature/list")
+    public ModelAndView list() {
+        Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT);
+        Map<String, String> features = jedis.hgetAll(FEATURES_MAP_KEY);
+        ModelAndView mv = new ModelAndView("feature_list");
+        mv.addObject("features", features);
         return mv;
     }
 }
